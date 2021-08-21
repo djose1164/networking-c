593bcf93a565e3cdcd52966145b38d8aa5a175ac
Jose S. Daniel <djose1164@gmail.com>
2021-08-21 19:48:10 -0400

    one module.

--- ch02/time_server.c
+++ ch02/time_server.c
@@ -1,6 +1,21 @@
-#include "../include/sock_init.h"
-#include "../include/socket.h"
-#include <string.h>
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif
 
 int main(int argc, char const *argv[])
 {
